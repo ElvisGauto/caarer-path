aad5e2eb56d141841d57226ee1797be6d60c030f
Mauro Gauto <mauro.gauto@globallogic.com>
2021-11-09 00:18:31 -0300

    section 3 finished

---
+++ JavaScript/Frameworks/React/react-pro/02-react-app/src/routes/Navigation.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    NavLink
+} from "react-router-dom";
+
+import logo from '../logo.svg';
+
+export const Navigation = () => {
+    return (
+      <Router>
+        <div className="main-layout">
+          <nav>
+              <img src={ logo } alt="React logo" />
+            <ul>
+              <li>
+                <NavLink
+                    to="/"
+                    end
+                    className={({ isActive }) =>
+                    (isActive ? "nav-active" : "")}>
+                    Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                    to="/about"
+                    end
+                    className={({ isActive }) =>
+                        (isActive ? "nav-active" : "")
+                    }
+                    >
+                    About
+                    </NavLink>
+              </li>
+              <li>
+                <NavLink
+                    to="/users"
+                    end
+                    className={({ isActive }) =>
+                        (isActive ? "nav-active" : "")
+                    }
+                >
+                    Users
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+   
+          <Routes>
+            <Route path="/about" element={ <h1>About</h1> } />
+            <Route path="/users" element={ <h1>Users</h1> } />
+            <Route path="/" element={ <h1>Home</h1> } />
+          </Routes>
+        </div>
+      </Router>
+    );
+}
