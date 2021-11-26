4479830a58f1113a49b219c8340dee04451048da
Mauro Gauto <mauro.gauto@globallogic.com>
2021-11-26 00:38:45 -0300

    section 7 finished: control props

--- JavaScript/Frameworks/React/react-pro/02-react-app/src/routes/02-component-patterns/pages/ShoppingPage.tsx
+++ JavaScript/Frameworks/React/react-pro/02-react-app/src/routes/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,16 +1,18 @@
 import { ProductButtons, ProductCard, ProductImage, ProductTitle } from '../components';
 
+import { useShoppingCart } from '../hooks/useShoppingCart';
+
+import { products } from '../data/product';
+
 import '../styles/custom-styles.css';
 
-const product = {
-    id: '1',
-    title: 'Coffe Mug - Card',
-    img: './coffee-mug.png'
-}
-
 export const ShoppingPage = () => {
+                                                        
+
+    const { shoppingCart, onProductCountChange } = useShoppingCart();
+
     return (
-        <div style={{ }}>
+        <div>
             <h1>Shopping Store</h1>
             <hr />
 
@@ -19,36 +21,56 @@ export const ShoppingPage = () => {
                 flexDirection: 'row',
                 flexWrap: 'wrap'
             }}>
-                {/* exportacion de componentes unificado */}
-                <ProductCard
-                    product={ product }
-                    className="bg-dark text-white"
-                >
-                    <ProductCard.Image className="custom-image" />
-                    <ProductCard.Title className="text-bold" />
-                    <ProductCard.Buttons className="custom-buttons" />
-                </ProductCard>
 
-                <ProductCard
-                    product={ product }
-                    className="bg-dark text-white"
-                >
-                    <ProductImage className="custom-image" />
-                    <ProductTitle className="text-bold" />
-                    <ProductButtons className="custom-buttons" />
-                </ProductCard>
-
-                <ProductCard
-                    product={ product }
-                    style={{
-                        backgroundColor: '#70D1F8'
-                    }}
-                >
-                    <ProductImage style={{ boxShadow: '10px 10px 10px rgba( 0, 0, 0, 0.2 )' }} />
-                    <ProductTitle style={{ fontWeight: 'bold' }} />
-                    <ProductButtons style={{ display: 'flex', justifyContent: 'end' }} />
-                </ProductCard>
+                {
+                    products.map( ( product ) => (
+                        <ProductCard
+                            className="bg-dark text-white"
+                            product={ product }
+                            key={ product.id }
+                            onChange={ onProductCountChange }
+                            value={ shoppingCart[product.id]?.count || 0 }
+                        >
+                            <ProductImage className="custom-image" />
+                            <ProductTitle className="text-bold" />
+                            <ProductButtons className="custom-buttons" />
+                        </ProductCard>
+                    ))
+                }
             </div>
+
+            <div className="shopping-cart">
+
+                {
+                    // sirve para obtener las entradas de un objeto y que podamos mapearlas
+                    Object.entries( shoppingCart ).map (([key, product]) => (
+                        <ProductCard
+                            key={ key }
+                            className="bg-dark text-white"
+                            product={ product }
+                            style={{ width: '100px' }}
+                            onChange={ onProductCountChange }
+                            value={ product.count }
+                        >
+                            <ProductImage className="custom-image" />
+                            <ProductButtons 
+                                className="custom-buttons"
+                                style={{
+                                    display: 'flex',
+                                    justifyContent: 'center'
+                                }}
+                            />
+                        </ProductCard>
+                    ))
+                }
+            </div>
+
+            <div>
+                <code>
+                    { JSON.stringify( shoppingCart, null, 5 ) }
+                </code>
+            </div>
+
         </div>
     )
 }
--- JavaScript/Frameworks/React/react-pro/notes.md
+++ JavaScript/Frameworks/React/react-pro/notes.md
@@ -6,5 +6,7 @@ Es la carga perezosa de modulos en react. Cargar componentes bajo demanda.
 
 Es una forma de hacer algo. Al aplicarse se obtienen beneficios. Por ejemplo Redux es un patron
 
-    - Compount Component Pattern: nos ayuda armar un componente y a partir de ese componente armar otro. Como un HOCs (Higher Order Components)
+    - Compount Component Pattern: nos ayuda armar un componente y a partir de ese componente armar otro. Como un HOCs (Higher Order Components).
+    - Control Props: propiedades que pueden generar cambios en otros props del componente. Por ejemplo en el caso del input, se necesita de la prop onChange para cambiar
+                     el valor del value.
                                   
